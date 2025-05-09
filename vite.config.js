4d5f216154fd4f17bf2f161fa7bc17d28f76f565
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-05-09 11:34:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy: {
       // Proxy API requests
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://ecommerce-backend-1-npbm.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
