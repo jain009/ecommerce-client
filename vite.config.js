ebb76338e360603ea13bc6a6608311f69e2ffcaf
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-05-12 17:48:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
   }
-});
+})
