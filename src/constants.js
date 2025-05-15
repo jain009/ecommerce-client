d1e176cfb852661fc60619c041361b9919ebe4cb
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-05-16 00:45:16 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL =  https://ecommerce-backend-1-npbm.onrender.com
+export const BASE_URL =  "https://ecommerce-backend-1-npbm.onrender.com"
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = "/api/users"; // Corrected URL
 export const ORDERS_URL = '/api/orders';
