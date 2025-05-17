1c3574eba719259a7427aaba449c532192c6aa2f
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-05-17 20:00:00 +0530

    Update apiSlices.js

--- src/slices/apiSlices.js
+++ src/slices/apiSlices.js
@@ -3,18 +3,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: BASE_URL,
-    credentials: 'include', //  Important for sending cookies (session data)
-    // You can add a prepareHeaders function here for common headers
+ credentials: 'include', // for cookies, if needed
     prepareHeaders: (headers, { getState }) => {
-       
-        headers.set('Content-Type', 'application/json'); //Set content type
-        return headers;
+      const token = getState().auth.userInfo?.token;
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
     },
-});
-
-export const apiSlice = createApi({
-    baseQuery,
-    tagTypes: ['Product', 'Order', 'User'],
-    endpoints: (builder) => ({}), 
-   
+  }),
+  endpoints: () => ({}),
 });
