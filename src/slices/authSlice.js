ead76f53a61267bf6289b83e7caab82e51e22d92
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-05-17 19:57:11 +0530

    Update authSlice.js

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    userInfo: localStorage.getItem('userInfo') 
-        ? JSON.parse(localStorage.getItem('userInfo')) 
-        : null,
+   const userInfo = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
 };
 
 const authSlice = createSlice({
